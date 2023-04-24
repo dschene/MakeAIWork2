20c802ba42f9d1b11c7946ee20a6be0396592e0d
dschene <1071325@hr.nl>
2023-04-24 11:14:22 +0200

    renamed train_models

--- project/Modules/train_model.py
+++
@@ -1,6 +0,0 @@
-from Models import Lidar_sweep
-from Models import Sonar_sweep
-import pandas as pd
-
-lidar_samples = pd.read_table('../Data/default_data', sep = ' ', header=None)
-#sonar_samples = 
