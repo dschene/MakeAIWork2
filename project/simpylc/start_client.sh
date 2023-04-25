3482996e69e4e91276152ca46b9fc203c2f63038
dschene <1071325@hr.nl>
2023-04-25 10:33:42 +0200

    sync

--- project/simpylc/start_client.sh
+++ project/simpylc/start_client.sh
@@ -5,6 +5,3 @@ export LIBGL_ALLOW_SOFTWARE=1
 
 <<<<<<< HEAD
 python simulations/car/control_client/hardcoded_client.py
-=======
-python simulations/car/control_client/my_agent.py
->>>>>>> 598dad2db34dfb17d169db88f9f4de035345d7bf
