ce7ef019490b1c0277f3089b203f520c6221d0af
dschene <1071325@hr.nl>
2023-04-25 14:46:25 +0200

    updated models

--- project/Modules/train_models.py
+++ project/Modules/train_models.py
@@ -31,7 +31,7 @@ Y_lidar = torch.Tensor(lidar_samples.iloc[:, -1:].to_numpy()).to(device)
 #Using DataLoader to iterate over the datasets and optionally shuffle + set batch size of training
 
 
-lidar_loader = DataLoader(list(zip(X_lidar, Y_lidar)), shuffle=False, batch_size=10)
+lidar_loader = DataLoader(list(zip(X_lidar, Y_lidar)), shuffle=True, batch_size=25)
 sonar_loader = DataLoader(list(zip(X_sonar, Y_sonar)), shuffle=True, batch_size=25)
 
 #######################################################
